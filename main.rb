1e0c4c9746b91cb9312ec1c8bdb8a2febcd3469c
IMP1 <IMP1@mail.com>
2019-09-10 14:39:53 +0100

    Add example to test pushing up ramps

--- main.rb
+++ main.rb
@@ -1,6 +1,14 @@
 require_relative 'base_blocks'
 require_relative 'runner'
 
+SISYPHUS = <<-END
+
+.....
+.  2.
+.  /.
+.>>..
+
+END
 
 CODE = <<-END
 
@@ -20,5 +28,5 @@ options = {
     delay: 0.2,
     trace: true,
 }
-runner = Vocal::Runner.new(CODE, "test_prog", [], options)
+runner = Vocal::Runner.new(SISYPHUS, "test_prog", [], options)
 runner.run
